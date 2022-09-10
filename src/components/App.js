28dd97ea3cc5c97a653091111a63ca8a9dd5ab93
Bailey Cage <bailey.cage@gmail.com>
2022-09-10 17:43:01 -0400

    implement spotify API to fetch user saved songs

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,86 @@
 import '../App.css';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import LoginButton from './LoginButton';
+import { Base64 } from 'js-base64';
+import { CLIENT_ID, CLIENT_SECRET } from '../keys';
+
 
 function App() {
+
+  const [searchParams] = useSearchParams();
+  let accessToken = '';
+
+  useEffect(() => {
+    if (searchParams.get('code')) fetchAccessToken();
+  }, []);
+
+  function fetchAccessToken () {
+    console.log('fetching access token ...');
+    const query = new URLSearchParams({
+      grant_type: "authorization_code",
+      code: searchParams.get('code'),
+      redirect_uri: "http://localhost:3000/index.html",
+      client_id: CLIENT_ID,
+    });
+    const url = `https://accounts.spotify.com/api/token?` + query;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + Base64.encode(CLIENT_ID + ':' + CLIENT_SECRET).toString()
+      },
+      body: query,
+    })
+    .then(r => r.json())
+    .then(data => {
+      // set tokens
+      accessToken = data.access_token;
+      // refreshToken = data.refresh_token;
+      // calc & set time to refresh
+      let now = new Date()
+      // expiration = now.setSeconds(now.getSeconds() + data.expires_in);
+    })
+    .then(() => {
+      fetchUserLibrary();
+    })
+    .catch(error => console.log(error))
+  }
+
+  function fetchUserLibrary () {
+    console.log('fetching user library ...');
+    const url = "https://api.spotify.com/v1/me/tracks";
+    fetch (url, {
+      headers: {
+        "Authorization": 'Bearer ' + accessToken,
+        "Content-Type": "application/json"
+      }
+    })
+    .then(r => r.json())
+    .then(data => {
+      console.log(data);
+      let limit = data.limit;
+      let numFetch = Math.ceil(data.total / data.limit) - 1;
+      for (let i=0; i<numFetch; ++i) {
+        const query = new URLSearchParams({
+          offset: limit * i,
+        })
+        const url = `https://api.spotify.com/v1/me/tracks?` + query;
+        fetch(url, {
+          headers: {
+            "Authorization": 'Bearer ' + accessToken,
+            "Content-Type": "application/json"
+          }
+        })
+        .then(r => r.json())
+        .then(data => {
+          console.log(data);
+          // store data into local server
+        })
+      }
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import { CLIENT_ID } from "../keys";
+import { CLIENT_ID } from '../keys';
 
 function LoginButton () {
+
+    function authorizeApp () {
+        let query = new URLSearchParams({
+            response_type: "code",
+            client_id: CLIENT_ID,
+            scope: "user-library-read",
+            redirect_uri: "http://localhost:3000/index.html",
+            state: genRandomString(20),
+            // code_challenge_method: "S256",
+            // code_challenge: "",
+            show_dialog: true,
+        })
+        let url = `https://accounts.spotify.com/authorize?` + query;
+        const loginPopup = window.open(url, 'popup', 'menubar=no,width=600,height=925');
+        const checkPopup = setInterval(() => {
+            if (loginPopup.window.location.href.includes("http://localhost:3000/index.html")) loginPopup.close();
+            if (!loginPopup || !loginPopup.closed) return;
+            clearInterval(checkPopup);
+            window.location.href = loginPopup.window.location.href;
+        }, 100);
+    }
+
+    function genRandomString(length) {
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const numPossible = possible.length;
+        let randString = '';
+        for (let i=0; i<length; ++i) {
+            randString += possible.charAt(Math.floor(Math.random() * numPossible))
+        }
+        return randString;
+    }
+
     return (
-        <button>login</button>
+        <button onClick={authorizeApp}>login</button>
     )
 }
 
