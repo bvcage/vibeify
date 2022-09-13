90ec01d1042df60d037eb12b6cba82b54e173a86
Bailey Cage <bailey.cage@gmail.com>
2022-09-13 19:30:30 -0400

    add Grid for styling

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,15 +1,37 @@
 import React from 'react'
-import { Card } from '@mui/material'
+import { Card, Grid } from '@mui/material'
+import "../App.css"
 
 function SongCard({ song }) {
-  const { name, album, artists } = song;
+  const { name, album, artists, url } = song;
   return (
     <Card sx={{ maxWidth: 345 }}>
-      {name}
-      <br />
-      {album.name}
-      <br />
-      {artists.map(artist => artist.name).join(", ")}
+
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+      >
+
+        <Grid item xs>
+          <img className="album-art" src={album.imageUrl} alt={'album art'} />
+        </Grid>
+
+        <Grid item xs>
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            >{name}
+          </a>
+          <br />
+          {album.name}
+          <br />
+          {artists.map(artist => artist.name).join(", ")}
+        </Grid>
+        
+      </Grid>
+
     </Card>
   )
 }
