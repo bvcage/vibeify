b57051a8c96dcae46a0b1ccda690d6b15fa49399
Bailey Cage <bailey.cage@gmail.com>
2022-09-13 12:34:36 -0400

    add useState for playlists list

--- src/components/PlaylistMenu.js
+++ src/components/PlaylistMenu.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GetSimilarForm from './GetSimilarForm'
 import PlaylistCard from './PlaylistCard'
 
 function PlaylistMenu() {
+
+  const [playlistAry, setPlaylistAry] = useState(loadPlaylistsFromDB);
+
+  function loadPlaylistsFromDB() {
+    fetch(`http://localhost:3001/playlists`)
+      .then(r => r.json())
+      .then(playlists => {
+        let tempAry = [];
+        playlists.forEach(list => tempAry.push(list))
+        setPlaylistAry([...tempAry]);
+      })
+  }
+
   return (
-    <div>PlaylistMenu</div>
+    <div>
+      <h2>PlaylistMenu</h2>
+    </div>
   )
 }
 
