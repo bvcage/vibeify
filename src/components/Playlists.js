12e999b7e8ec66b38e8eb249b3becd9c958873ce
Bailey Cage <bailey.cage@gmail.com>
2022-09-13 12:34:10 -0400

    add useState for songs list

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlaylistMenu from './PlaylistMenu'
 import SongsList from './SongsList'
 
 function Playlists() {
+
+  const [fullSongIdAry, setFullSongIdAry] = useState(loadSongsFromDB);
+  const [selectedPlaylist, setSelectedPlaylist] = useState('');
+  const [songsToDisplay, setSongsToDisplay] = useState([]);
+
+  function loadSongsFromDB() {
+    fetch(`http://localhost:3001/songs`)
+      .then(r => r.json())
+      .then(library => {
+        let tempAry = [];
+        library.forEach(song => tempAry.push(song))
+        setFullSongIdAry(tempAry);
+      })
+  }
+
   return (
     <div>
       <h2>Playlists</h2>
       <PlaylistMenu />
-      <SongsList />
+      <SongsList songs={songsToDisplay} />
     </div>
   )
 }
