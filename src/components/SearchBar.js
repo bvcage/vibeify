9adf5ff44f667f29d6d43a3724b8127ffd48c921
Bailey Cage <bailey.cage@gmail.com>
2022-09-15 12:54:51 -0400

    update fetch to reflect new DB structure

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,47 +5,48 @@ import Button from '@mui/material/Button'
 import { Autocomplete } from '@mui/material';
 
 function SearchBar({ onClickAdd }) {
-    const [songs, setSongs] = useState([])
-    const [open, setOpen] = useState(false)
-    let searchTerm = ''
+  const userId = localStorage.getItem("user_id");
+  const [songs, setSongs] = useState([])
+  const [open, setOpen] = useState(false)
+  let searchTerm = ''
 
 
-    useEffect(() => {
-        fetch('http://localhost:3001/songs')
-        .then((res) => res.json())
-        .then((data) => setSongs(data))
-    }, [])
+  useEffect(() => {
+      fetch(`http://localhost:3001/users/${userId}`)
+      .then((res) => res.json())
+      .then((user) => setSongs(user.songs))
+  }, [userId])
 
-    return (
-      <Autocomplete
-        value={searchTerm}  
-        selectOnFocus
-        clearOnBlur
-        handleHomeEndKeys
-        options={songs}
-        getOptionLabel={(option) => {
-          if (typeof option === 'string') {
-            return option;
-          }
-          return (option.artists[0].name + option.name)
-        }}
-        open={open}
-        onInputChange={(_, searchTerm) => {
-          if (searchTerm.length === 0) {
-            if (open) setOpen(false);
-          } else {
-            if (!open) setOpen(true);
-          }
-        }}
-        onClose={() => setOpen(false)}
-        renderOption={(props, option) => <Box><Button onClick={() => onClickAdd(option)}>+ Add</Button>{option.name} - {option.artists[0].name}</Box>}
-        sx={{ width: 1152, paddingTop: '15px' }}
-        freeSolo
-        renderInput={(params) => (
-          <TextField {...params} label="Search for a song..." />
-        )}
-      />
-    );
+  return (
+    <Autocomplete
+      value={searchTerm}  
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      options={songs}
+      getOptionLabel={(option) => {
+        if (typeof option === 'string') {
+          return option;
+        }
+        return (option.artists[0].name + option.name)
+      }}
+      open={open}
+      onInputChange={(_, searchTerm) => {
+        if (searchTerm.length === 0) {
+          if (open) setOpen(false);
+        } else {
+          if (!open) setOpen(true);
+        }
+      }}
+      onClose={() => setOpen(false)}
+      renderOption={(props, option) => <Box><Button onClick={() => onClickAdd(option)}>+ Add</Button>{option.name} - {option.artists[0].name}</Box>}
+      sx={{ width: 1152, paddingTop: '15px' }}
+      freeSolo
+      renderInput={(params) => (
+        <TextField {...params} label="Search for a song..." />
+      )}
+    />
+  );
 }
 
 export default SearchBar
