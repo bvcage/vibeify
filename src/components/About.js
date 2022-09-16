f68d1a14007be6efca8adf304a260f8a36c99f6f
AJ <ajc_cav07@yahoo.com>
2022-09-16 08:16:49 -0500

    about added

--- src/components/About.js
+++ src/components/About.js
@@ -11,13 +11,13 @@ function About() {
       <Paper>
         <Box p={5}>
           <div>Vibeify</div>
-          <Box pt={5}>Create a playlist base on your mood!</Box>
-          <Box pt={5}>Vibeify About Me</Box>
-          <Box pt={5}>Vibeify About Me</Box>
-          <Box pt={5}>Vibeify About Me</Box>
-          <Box pt={5}>Vibeify About Me</Box>
-          <Box pt={5}>Vibeify About Me</Box>
-          <Box pt={5}>Vibeify About Me</Box>
+          <Box pt={5}>Users are able to login using personal Spotify account accessing their own playlist</Box>
+          <Box pt={5}>Create your playlist to match your mood for the day!</Box>
+          <Box pt={5}>Click on your song and Spotify will play it</Box>
+          <Box pt={5}>Users can search up new songs and add or delete them accordingly</Box>
+          <Box pt={5}>Like Vibeify? Checkout out the developers Github who created it in the footer!</Box>
+          <Box pt={5}>Users are able to Log back out when they are done listening</Box>
+          <Box pt={5}>Match your vibe with no monthly subscription</Box>
           </Box>
       </Paper>
     </Box>
