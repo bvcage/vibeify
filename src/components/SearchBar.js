66c8ecdc8cb34e98f31f8ad0cedec5e790de3378
Bailey Cage <bailey.cage@gmail.com>
2022-09-16 10:08:26 -0400

    clean up

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import { Autocomplete, Box } from '@mui/material';
@@ -12,11 +12,11 @@ function SearchBar({ onClickAdd }) {
 
   let searchTerm = ''
 
-    const darkTheme = createTheme({
-      palette: {
-        mode: 'dark',
-      },
-    });
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   useEffect(() => {
       fetch(`http://localhost:3001/users/${userId}`)
@@ -24,39 +24,39 @@ function SearchBar({ onClickAdd }) {
       .then((user) => setSongs(user.songs))
   }, [userId])
 
-    return (
-      <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Autocomplete
-        value={searchTerm}  
-        selectOnFocus
-        clearOnBlur
-        handleHomeEndKeys
-        options={songs}
-        getOptionLabel={(option) => {
-          if (typeof option === 'string') {
-            return option;
-          }
-          return (option.artists[0].name + option.name)
-        }}
-        open={open}
-        onInputChange={(_, searchTerm) => {
-          if (searchTerm.length === 0) {
-            if (open) setOpen(false);
-          } else {
-            if (!open) setOpen(true);
-          }
-        }}
-        onClose={() => setOpen(false)}
-        renderOption={(props, option) => <Box><Button onClick={() => onClickAdd(option)}>+ Add</Button>{option.name} - {option.artists[0].name}</Box>}
-        sx={{ width: 1152, paddingTop: '15px' }}
-        freeSolo
-        renderInput={(params) => (
-          <TextField {...params} label="Search for a song..." />
-        )}
-      />
-      </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <Autocomplete
+      value={searchTerm}  
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      options={songs}
+      getOptionLabel={(option) => {
+        if (typeof option === 'string') {
+          return option;
+        }
+        return (option.artists[0].name + option.name)
+      }}
+      open={open}
+      onInputChange={(_, searchTerm) => {
+        if (searchTerm.length === 0) {
+          if (open) setOpen(false);
+        } else {
+          if (!open) setOpen(true);
+        }
+      }}
+      onClose={() => setOpen(false)}
+      renderOption={(props, option) => <Box key={option.id}><Button onClick={() => onClickAdd(option)}>+ Add</Button>{option.name} - {option.artists[0].name}</Box>}
+      sx={{ width: 1152, paddingTop: '15px' }}
+      freeSolo
+      renderInput={(params) => (
+        <TextField {...params} label="Search for a song..." />
+      )}
+    />
+    </ThemeProvider>
+  );
 }
 
 export default SearchBar
