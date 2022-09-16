8d6301f3a10159e0750fbc0ab7791162f646751d
Bailey Cage <bailey.cage@gmail.com>
2022-09-16 09:24:58 -0400

    clean up

--- src/components/PlaylistMenu.js
+++ src/components/PlaylistMenu.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import PlaylistCard from "./PlaylistCard";
-import { Container, Grid, Card } from "@mui/material";
+import { Grid } from "@mui/material";
 
 import images from "../data/playlistImages.json"
 
 function PlaylistMenu({ playlistAry, onClickPlaylist }) {
-
-
   return (
     <Grid sx={{width: 1000, marginLeft: '128px'}}>
       {playlistAry.map((playlist) => {
