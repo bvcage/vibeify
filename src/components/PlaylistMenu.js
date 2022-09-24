e751dcc49248c7536dacaecf3af52f07759d5fc7
Bailey Cage <bailey.cage@gmail.com>
2022-09-24 18:31:56 -0400

    revert from image to regular button

--- src/components/PlaylistMenu.js
+++ src/components/PlaylistMenu.js
@@ -2,23 +2,22 @@ import React from "react";
 import PlaylistCard from "./PlaylistCard";
 import { Grid } from "@mui/material";
 
-import images from "../data/playlistImages.json"
-
 function PlaylistMenu({ playlistAry, onClickPlaylist }) {
 
   const playlistCards = playlistAry.map((playlist) => {
     return (
+      <Grid item xs={3} key={playlist.id}>
         <PlaylistCard
           key={playlist.id}
           playlist={playlist}
           onClickPlaylist={onClickPlaylist}
-          image={images[playlist.id]}
         />
+      </Grid>
     );
   })
 
   return (
-    <Grid sx={{width: 1000, marginLeft: '128px'}}>
+    <Grid container spacing={1}>
       {playlistCards}
     </Grid>
   );
