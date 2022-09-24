949c8212c0d0e4ba8453a58e7478ac1be4393e9f
Bailey Cage <bailey.cage@gmail.com>
2022-09-24 19:19:08 -0400

    load & display user's playlists

--- src/components/PlaylistMergeForm.js
+++ src/components/PlaylistMergeForm.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
+import { loadSpotifyPlaylistsAry } from '../scripts/localDB'
+
 function PlaylistMergeForm () {
+
+   const [spotifyPlaylistsAry, setSpotifyPlaylistsAry] = useState([]);
+
+   useEffect(() => {
+      loadSpotifyPlaylistsAry()
+      .then(data => setSpotifyPlaylistsAry(data));
+   }, [])
+
+   const playlistsList = spotifyPlaylistsAry.map(playlist => {
+      return (
+         <li>{playlist.name}</li>
+      )
+   });
+
    return (
-      <>merge form</>
+      <ul>{playlistsList}</ul>
    )
 }
 
