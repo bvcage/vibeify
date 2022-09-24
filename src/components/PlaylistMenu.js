68c1cfe8a6f2dc40a670d1eadd56426f249cfe4b
Bailey Cage <bailey.cage@gmail.com>
2022-09-24 17:25:26 -0400

    move playlist cards map function  out of return

--- src/components/PlaylistMenu.js
+++ src/components/PlaylistMenu.js
@@ -5,18 +5,21 @@ import { Grid } from "@mui/material";
 import images from "../data/playlistImages.json"
 
 function PlaylistMenu({ playlistAry, onClickPlaylist }) {
+
+  const playlistCards = playlistAry.map((playlist) => {
+    return (
+        <PlaylistCard
+          key={playlist.id}
+          playlist={playlist}
+          onClickPlaylist={onClickPlaylist}
+          image={images[playlist.id]}
+        />
+    );
+  })
+
   return (
     <Grid sx={{width: 1000, marginLeft: '128px'}}>
-      {playlistAry.map((playlist) => {
-        return (
-            <PlaylistCard
-              key={playlist.id}
-              playlist={playlist}
-              onClickPlaylist={onClickPlaylist}
-              image={images[playlist.id]}
-            />
-        );
-      })}
+      {playlistCards}
     </Grid>
   );
 }
