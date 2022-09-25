d8862e180a5a5e46c017ef682e7bcd372c675da5
Bailey Cage <bailey.cage@gmail.com>
2022-09-25 15:45:05 -0400

    make playlists selectable via checkbox; add submit function to merge selected playlists

--- src/components/PlaylistMergeForm.js
+++ src/components/PlaylistMergeForm.js
@@ -1,23 +1,53 @@
 import { useEffect, useState } from 'react';
 import { loadSpotifyPlaylistsAry } from '../scripts/localDB'
+import { Button, Checkbox, FormControlLabel, FormGroup } from '@mui/material'
+import { createMergePlaylist } from '../scripts/createPlaylists'
 
-function PlaylistMergeForm () {
+function PlaylistMergeForm ({ onSubmit }) {
 
    const [spotifyPlaylistsAry, setSpotifyPlaylistsAry] = useState([]);
+   const [formValues, setFormValues] = useState({});
 
    useEffect(() => {
       loadSpotifyPlaylistsAry()
       .then(data => setSpotifyPlaylistsAry(data));
    }, [])
 
+   function handleCheckbox (event) {
+      if (event.target.checked) {
+         setFormValues({...formValues,
+            [event.target.value]: event.target.checked
+         })
+      } else {
+         let temp = {...formValues}
+         delete temp[event.target.value]
+         setFormValues(temp)
+      }
+   }
+
+   function handleSubmit (event) {
+      event.preventDefault();
+      createMergePlaylist(Object.keys(formValues))
+      .then(playlist => onSubmit(playlist))
+   }
+
    const playlistsList = spotifyPlaylistsAry.map(playlist => {
       return (
-         <li>{playlist.name}</li>
+         <FormControlLabel
+            key={playlist.id}
+            control={<Checkbox value={playlist.id} label={playlist.name} onChange={handleCheckbox} />}
+            label={playlist.name}
+         />
       )
    });
 
    return (
-      <ul>{playlistsList}</ul>
+      <form onSubmit={handleSubmit}>
+         <FormGroup>
+            {playlistsList}
+            <Button type="submit">submit</Button>
+         </FormGroup>
+      </form>
    )
 }
 
