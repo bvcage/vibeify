849d7e5ca0d538837d0712b32b4c533ebdc90af4
Bailey Cage <bailey.cage@gmail.com>
2022-10-15 17:13:31 -0400

    js to jsx; refactor for API backend

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '@mui/material/Button';
+import { authorizeVibeify } from '../scripts/spotify'
+import { saveToSinatra } from '../scripts/sinatra'
+
+
+function LoginButton () {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("user_id") !== null);
+    const navigate = useNavigate();
+
+    async function toggleLoginLogout () {
+        if (isLoggedIn) {
+            // clearDB()
+            // .then(() => {
+                localStorage.clear();
+                setIsLoggedIn(!isLoggedIn);
+                window.location.replace('http://localhost:3000')
+            // })
+        }
+        else {
+            const user = await authorizeVibeify()
+            saveToSinatra('/users', user)
+            setIsLoggedIn(!isLoggedIn)
+            navigate('/loading')
+        }
+    }
+
+    return (
+        <Button
+            sx={{textTransform: 'none'}}
+            variant='contained'
+            color='success'
+            onClick={toggleLoginLogout}
+            >{localStorage.getItem("user_id") ? 'Logout' : 'Login with Spotify'}
+        </Button>
+    )
+
+}
+
+export default LoginButton
