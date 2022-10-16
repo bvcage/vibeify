b1784918b2ef497e2018dfe145603a8491f54218
Bailey Cage <bailey.cage@gmail.com>
2022-10-16 01:39:57 -0400

    reimplement fetch & delete for new backend

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import '../App.css'
+import { Box, Stack } from '@mui/material'
 import PlaylistsMenu from './PlaylistsMenu'
+import PlaylistsMergeForm from './PlaylistsMergeForm'
 import SongsList from './SongsList'
-import { addSongToPlaylist, removeSongFromPlaylist } from '../scripts/localDB';
-import { Box, Stack } from '@mui/material';
-import SearchBar from './SearchBar';
-import PlaylistsMergeForm from './PlaylistsMergeForm';
+import SearchBar from './SearchBar'
+
 
 function Playlists () {
 
@@ -15,52 +14,65 @@ function Playlists () {
   const [songsAry, setSongsAry] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/playlists')
+    fetch('http://localhost:9292/playlists/defaults')
     .then(r=>r.json())
     .then(data => setPlaylistAry(data))
   }, [])
 
-  function onClickDelete (songId) {
-    const patchSongsAry = removeSongFromPlaylist(selectedPlaylist, songId);
-    const patchPlaylist = {...selectedPlaylist,
-      "tracks": patchSongsAry
-    }
-    setPlaylistAry(patchPlaylist);
-    setSelectedPlaylist(patchPlaylist);
-    setSongsAry(patchSongsAry);
+  function onClickDelete (songToDelete) {
+    const songs = selectedPlaylist.songs.filter(song => song.id !== songToDelete.id)
+    const playlist = {...selectedPlaylist, songs:songs}
+    setSongsAry(songs)
+    setSelectedPlaylist(playlist)
+    setPlaylistAry(playlistAry.map(existing => {
+      if (existing.id === playlist.id) { return playlist }
+      return existing
+    }))
+    fetch('http://localhost:9292/saves', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        song_id: songToDelete.id,
+        playlist_id: playlist.id
+      })
+    }).then(() => console.log('deleted'))
   }
 
   function onClickMerge (playlist) {
     setSongsAry(playlist.songs);
     setSelectedPlaylist(playlist);
-    // setShowSongs(true);
+    setPlaylistAry([...playlistAry, playlist])
+    setShowSongs(true);
   }
 
   function onClickPlaylist (playlist) {
     if (playlist !== selectedPlaylist) {
       setShowSongs(true);
       setSelectedPlaylist(playlist);
-      setSongsAry(playlist.tracks);
+      setSongsAry(playlist.songs);
     } else {
       setShowSongs(!showSongs);
     }
   }
 
   function onClickAdd (song) {
-    const newSongsAry = addSongToPlaylist(selectedPlaylist, song);
-    const newSongPlaylist = {...selectedPlaylist,
-      "tracks": newSongsAry
-    }
-    setPlaylistAry(newSongPlaylist);
-    setSelectedPlaylist(newSongPlaylist);
-    setSongsAry(newSongsAry);
+    console.log(song)
+    // const newSongsAry = addSongToPlaylist(selectedPlaylist, song);
+    // const newSongPlaylist = {...selectedPlaylist,
+    //   "songs": newSongsAry
+    // }
+    // setPlaylistAry(newSongPlaylist);
+    // setSelectedPlaylist(newSongPlaylist);
+    // setSongsAry(newSongsAry);
   };
 
   return (
     <Stack spacing={2}>
       <Box>
         <PlaylistsMenu playlistAry={playlistAry} onClickPlaylist={onClickPlaylist} />
-        <SearchBar onClickAdd={onClickAdd} />
+        {/* <SearchBar onClickAdd={onClickAdd} /> */}
       </Box>
       <Box sx={{pb: 28}}>
         {selectedPlaylist && selectedPlaylist.id === "merge" ? <PlaylistsMergeForm onSubmit={onClickMerge} /> : <SongsList songsAry={songsAry} showSongs={showSongs} onClickDelete={onClickDelete} />}
